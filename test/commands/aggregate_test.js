2ecfc24374c68c98991e3bff2cf756b558025b7c
Vlad Losev <vlad@visualsupply.co>
2015-05-30 17:09:00 -0700

    Adds recursive validation to $ifNull.

--- test/commands/aggregate_test.js
+++ test/commands/aggregate_test.js
@@ -717,6 +717,15 @@ describe('aggregate', function() {
         done);
     });
 
+    it('rejects parameter if non-operator object', function(done) {
+      assertAggregationError(
+        [],
+        [{'$group': {_id: '$a', total: {'$sum': {'$ifNull': {a: 1}}}}}],
+        16420,
+        'exception: field inclusion is not allowed inside of $expressions',
+        done);
+    });
+
     it('rejects non-array parameters', function(done) {
       assertAggregationError(
         [],
